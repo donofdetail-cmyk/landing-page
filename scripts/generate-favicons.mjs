42f22cb889c3a03e38d302ab9c9cda992086de45
Logan Wheeler <logan@MacBookPro.lan>
2026-06-03 20:40:23 -0700

    chore: Update favicons to use new D-O-N icon shape

--- scripts/generate-favicons.mjs
+++ scripts/generate-favicons.mjs
@@ -6,7 +6,7 @@ const svg = `<svg viewBox="0 0 140 140" xmlns="http://www.w3.org/2000/svg">
   <g transform="translate(20, 20)">
     <path fill="#1A1A1A" fill-rule="evenodd" clip-rule="evenodd" d="M 0 25 L 25 12.5 L 25 87.5 L 0 75 Z M 8 30 L 17 25.5 L 17 74.5 L 8 70 Z" />
     <path fill="#1A1A1A" fill-rule="evenodd" clip-rule="evenodd" d="M 50 0 L 67 8.5 L 67 91.5 L 50 100 L 33 91.5 L 33 8.5 Z M 50 9 L 59 13.5 L 59 86.5 L 50 91 L 41 86.5 L 41 13.5 Z" />
-    <path fill="#1A1A1A" fill-rule="evenodd" clip-rule="evenodd" d="M 75 12.5 L 100 25 L 100 75 L 75 87.5 Z M 83 25.5 L 92 30 L 92 70 L 83 74.5 Z" />
+    <path fill="#1A1A1A" d="M 75 87.5 L 75 12.5 L 83 16.5 L 92 47.5 L 92 21 L 100 25 L 100 75 L 92 79 L 83 43.8 L 83 83.5 Z" />
   </g>
 </svg>`;
 
